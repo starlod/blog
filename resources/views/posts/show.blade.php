6ed46c13cfd577f9926c7c48234870a5bb893897
starlod <starlod.ggl@gmail.com>
2017-11-11 00:35:30 +0900

    コメント一覧

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,5 +9,15 @@
         {{ $post->title }}
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
+
+    <h2>Comments</h2>
+
+    <ul>
+        @forelse($post->comments() as $comment)
+            <li>{{ $comment->body }}</li>
+        @empty
+            <li>No comments yet</li>
+        @endforelse
+    </ul>
 </div>
 @endsection
