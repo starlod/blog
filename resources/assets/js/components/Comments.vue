290eb6540cbeb65dd81fade58d0033d30b4046e1
starlod <starlod.ggl@gmail.com>
2017-12-20 17:04:43 +0900

    コメント

--- resources/assets/js/components/Comments.vue
+++ resources/assets/js/components/Comments.vue
@@ -1,23 +1,53 @@
 <template>
     <div class="comments">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+        <h2>Comments</h2>
 
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
+        <div class="comment" v-for="comment in comments" :key="comment.id">
+            <div class="comment-author">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <b>{{ comment.author_name }}</b>
+                <span>{{ comment.created_at }}</span>
             </div>
+            <div class="comment-body">{{ comment.body }}</div>
         </div>
     </div>
 </template>
 
+<style lang="scss">
+.comments {
+    margin: 20px 0;
+    padding: 20px 20px 20px 20px;
+    background-color: #eee;
+}
+.comment {
+    margin-bottom: 20px;
+}
+</style>
+
 <script>
     export default {
+        props: {
+            postId: Number
+        },
+        data() {
+            return {
+                json: '',
+                comments: {}
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            console.log('components.comments');
+            var self = this;
+            let url = '/api' + this.$route.path + '/comments';
+
+            console.log('axios.get ' + url);
+
+            axios.get(url).then(function (response) {
+                self.json = response.request.responseText;
+                self.comments = JSON.parse(self.json);
+            }).catch(function (error) {
+                console.error(error);
+            });
         }
     }
 </script>
